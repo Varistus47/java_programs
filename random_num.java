3e46d851be4feb5315d5f357f9368076951e5dcf
Varistus47 <106617396+Varistus47@users.noreply.github.com>
2022-11-30 23:04:13 +0530

    Add files via upload

---
+++ random_num.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+Random random=new Random();
+System.out.println("enter no of length:");
+int len=scan.nextInt();
+System.out.println("the password is:");
+for(int i=0;i<len;i++){
+System.out.print(random.nextInt(10));
+} 
+}
+}
+
